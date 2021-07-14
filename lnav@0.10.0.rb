2439d847db60c97f0af0ebba35702ae306b9cbc4
Alex Conlin <458604+alexconlin@users.noreply.github.com>
2021-07-14 10:08:44 +0100

    Add lnav@0.10.0

---
+++ lnav@0.10.0.rb
@@ -0,0 +1,37 @@
+class Lnav < Formula
+  desc "Curses-based tool for viewing and analyzing log files"
+  homepage "https://lnav.org/"
+  url "https://github.com/tstack/lnav/releases/download/v0.10.0-beta1/lnav-0.10.0.tar.gz"
+  sha256 "4feca9ac07a2b82c92750a124026d7a364bc5b3ce95d6eebff6bee52f91e21d7"
+  license "BSD-2-Clause"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  head do
+    url "https://github.com/tstack/lnav.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "re2c" => :build
+  end
+
+  depends_on "pcre"
+  depends_on "readline"
+  depends_on "sqlite"
+
+  def install
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--with-sqlite=#{Formula["sqlite"].opt_prefix}",
+                          "--with-readline=#{Formula["readline"].opt_prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/lnav", "-V"
+  end
+end
